5d16308ed3c72c2c043ac6d6f3bff9066e0b5fbe
Joe Westcott <joe@joewestcott.info>
2015-08-27 16:21:36 -0400

    custom text field for local translation
    new value, "text", which can be used for local translations

--- wp-content/plugins/itdp-search/itdp-search.php
+++ wp-content/plugins/itdp-search/itdp-search.php
@@ -2,10 +2,11 @@
 /*
 Plugin Name: ITDP Search Shortcode Plugin
 Description: Add a search box anywhere with the [itdp-search] shortcode. You can optionally choose the category with the "category_name=" option. For example, [itdp-search category_name="cycling-and-walking"]
-Version: 1.02
+Version: 1.03
 Author: Joe Westcott
 Author URI: http://www.joewestcott.info/
 Release Notes:
+1.03 custom text field for local translation
 1.02 custom action option
 1.01 corrected description, variable is category_name
 1.0 release
@@ -19,14 +20,15 @@ function itdp_search_shortcode( $atts , $content = null ) {
 	extract( shortcode_atts(
 		array(
 			'category_name' => '',
-			'action' => '/', 
+			'action' => '/',
+			'text' => 'Search ITDP ...',
 			/* previously action="https://www.itdp.org/" */
 		), $atts )
 	);
 
 	// Code
 
-return '<form action="' . $action . '" class="search-form" method="get" role="search"><input name="s" placeholder="Search ITDP ..." type="search"><input class="search-field" type="hidden" name="category_name" value="' . $category_name . '"><input class="search-submit" type="submit" value="Search"></form>';
+return '<form action="' . $action . '" class="search-form" method="get" role="search"><input name="s" placeholder="' . $text . '" type="search"><input class="search-field" type="hidden" name="category_name" value="' . $category_name . '"><input class="search-submit" type="submit" value="Search"></form>';
 
 }
 
